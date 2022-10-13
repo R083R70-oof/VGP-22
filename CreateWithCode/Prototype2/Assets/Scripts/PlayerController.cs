b9adfc471893f2c84d6b048e32eb017da8e9a304
R083R70-oof <113127001+R083R70-oof@users.noreply.github.com>
2022-10-13 09:14:32 -0700

    uyuyu

--- CreateWithCode/Prototype2/Assets/Scripts/PlayerController.cs
+++ CreateWithCode/Prototype2/Assets/Scripts/PlayerController.cs
@@ -31,6 +31,7 @@ public class PlayerController : MonoBehaviour
     
        horizontalInput = Input.GetAxis("Horizontal");
        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+       if (Input.GetKeyDown(KeyCode.Space))
        {
         Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
        }
