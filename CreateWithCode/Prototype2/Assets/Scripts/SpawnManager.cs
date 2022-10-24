7ea4a604cd00bd64b23fbf5183801676ea28d6a3
R083R70-oof <113127001+R083R70-oof@users.noreply.github.com>
2022-10-24 09:08:47 -0700

    yh6uiy

--- CreateWithCode/Prototype2/Assets/Scripts/SpawnManager.cs
+++ CreateWithCode/Prototype2/Assets/Scripts/SpawnManager.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject[] animalPrefabs;
+    public GameObject [] animalPrefabs;
+    public int animalIndex;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
 
     // Start is called before the first frame update
     void Start()
@@ -15,11 +18,16 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      if (Input.GetKeyDown(KeyCode.S)) 
+      if (Input.GetKeyDown(KeyCode.S))
       {
+        Spawn
+      }
+      {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),0, spawnPosZ);
+
         int animalIndex = Random.Range(0, animalPrefabs.Length);
         
-        Instantiate(animalPrefabs[animalIndex], new Vector3( 0, 0, 20),
+        Instantiate(animalPrefabs[animalIndex], spawnPos,
         animalPrefabs[animalIndex].transform.rotation);
       }   
     }
