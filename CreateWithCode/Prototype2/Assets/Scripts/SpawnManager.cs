888b51222b417ab794aeb29742d6bb6553290f99
R083R70-oof <113127001+R083R70-oof@users.noreply.github.com>
2022-10-26 09:11:30 -0700

    rhthe

--- CreateWithCode/Prototype2/Assets/Scripts/SpawnManager.cs
+++ CreateWithCode/Prototype2/Assets/Scripts/SpawnManager.cs
@@ -8,27 +8,26 @@ public class SpawnManager : MonoBehaviour
     public int animalIndex;
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+      InvokeRepeating("SpawnRandomAnimal", 2, 1.5f);    
     }
 
     // Update is called once per frame
     void Update()
-    {
-      if (Input.GetKeyDown(KeyCode.S))
+   
       {
-        Spawn
+      
       }
-      {
-        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),0, spawnPosZ);
-
+    
+    void SpawnRandomAnimal()
+    {
         int animalIndex = Random.Range(0, animalPrefabs.Length);
-        
-        Instantiate(animalPrefabs[animalIndex], spawnPos,
-        animalPrefabs[animalIndex].transform.rotation);
-      }   
+         Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),0, spawnPosZ);
+        Instantiate(animalPrefabs[animalIndex], spawnPos,animalPrefabs[animalIndex].transform.rotation);
     }
 }
