c716fcce3abb63d1da10ef2540f05a3c2d7f2d76
R083R70-oof <113127001+R083R70-oof@users.noreply.github.com>
2022-11-04 08:24:11 -0700

    fr

--- CreateWithCode/Plane/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ CreateWithCode/Plane/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public float speed;
+    public float speed =5f;
     public float rotationSpeed;
     public float verticalInput;
 
@@ -24,6 +24,6 @@ public class PlayerControllerX : MonoBehaviour
         transform.Translate(Vector3.forward * speed);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * speed * verticalInput);
     }
 }
