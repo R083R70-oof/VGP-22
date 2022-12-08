27b9403f94f1d1e8f4f387bed16655db8016cde7
R083R70-oof <113127001+R083R70-oof@users.noreply.github.com>
2022-12-08 09:07:02 -0800

    gun

--- CreateWithCode/PersonalProject/Assets/Scripts/MovePlayer.cs
+++ CreateWithCode/PersonalProject/Assets/Scripts/MovePlayer.cs
@@ -4,19 +4,40 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
+  private float speed = 20.0f;
+    private float Speed = 50;
+    private float horizontalInput;
+    private float forwardInput;
+     private Rigidbody playerRb;
+     public float gravityModifier;
+      public GameObject projectilePrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier; 
     }
 
     // Update is called once per frame
     void Update()
     {
+      horizontalInput = Input.GetAxis("Horizontal");
+      forwardInput = Input.GetAxis("Vertical");
+      
       if(Input.GetKeyDown(KeyCode.Space))
       {
-         playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
-         isOnGround = false;
-      }   
+         playerRb.AddForce(Vector3.left * 10, ForceMode.Impulse);
+      }
+
+      // we move the vehicle forward 
+    transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+    // rotates the car based on horizontal Input
+    transform.Rotate(Vector3.left * horizontalInput * Time.deltaTime * Speed);
     }
+
+    if (Input.GetKeyDown(KeyCode.Space))
+       {
+        Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+       }
 }
