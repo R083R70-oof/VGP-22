f62010d87c69e5a2323bc40be68406639adffdb9
R083R70-oof <113127001+R083R70-oof@users.noreply.github.com>
2023-03-07 09:08:18 -0800

    scedfyjtjn

--- CreateWithCode/Prototype 4/Assets/Scripts/Enemy.cs
+++ CreateWithCode/Prototype 4/Assets/Scripts/Enemy.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public float speed = 3.0f;
+    private Rigidbody enemyRb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       enemyRb = GetComponent 
     }
 
     // Update is called once per frame
