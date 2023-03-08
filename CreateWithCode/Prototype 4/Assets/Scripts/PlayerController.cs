c422279af55540a7a85f5ddf660e86e88113c263
R083R70-oof <113127001+R083R70-oof@users.noreply.github.com>
2023-03-08 09:50:16 -0800

    qrt35yghr

--- CreateWithCode/Prototype 4/Assets/Scripts/PlayerController.cs
+++ CreateWithCode/Prototype 4/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     private GameObject focalPoint;
     public float speed = 5.0f;
+    public bool hasPowerup = false:
 
     // Start is called before the first frame update
     void Start()
@@ -22,4 +23,14 @@ public class PlayerController : MonoBehaviour
 
        playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
     }
+
+    private void OnTriggerEnter(collider other)
+    {
+      if(other.CompareTag("Powerup"))
+      {
+         
+         Destroy(other.gameObject);
+      }
+    }
+   
 }
---
+++ CreateWithCode/Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPreFab;
+    private float spawnRange = 9.0f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+       Instantiate(enemyPreFab, GenerateSpawnPosition(), enemyPreFab.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private Vector3 GenerateSpawnPosition()
+    {  
+    float spawnPosx = Random.Range(-spawnRange, spawnRange);
+    float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+
+        Vector3 randomPos = new Vector3(spawnPosx, 0, spawnPosZ);
+
+        return randomPos;
+    }
+}
+
