c6fc8fe0ef18e0998b84d09b5a5dc3873b7b7fb8
R083R70-oof <113127001+R083R70-oof@users.noreply.github.com>
2023-03-09 08:49:06 -0800

    sertdrgyui

---
+++ CreateWithCode/Prototype 5/Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    private float spawnRate = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       StartCoroutine() 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
