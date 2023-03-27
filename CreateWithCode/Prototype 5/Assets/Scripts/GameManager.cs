0d0fa88974a49f93c16b5c1669f7031094425f1a
R083R70-oof <113127001+R083R70-oof@users.noreply.github.com>
2023-03-27 09:03:40 -0700

    xsca

--- CreateWithCode/Prototype 5/Assets/Scripts/GameManager.cs
+++ CreateWithCode/Prototype 5/Assets/Scripts/GameManager.cs
@@ -12,31 +12,32 @@ public class GameManager : MonoBehaviour
 
     // Start is called before the first frame update
     void Start()
-    {
-       StartCoroutine(SpawnTarget());
-       UpdateScore(0);
-    }
+   {
+    StartCoroutine(SpawnTarget());
+    score = 0;
+    UpdateScore(0);
+   }
 
     // Update is called once per frame
     void Update()
-    {
+   {
         
-    }
+   }
 
 
     IEnumerator SpawnTarget()
-    {
-       while(true)
-       {
-        yield return new WaitForSeconds(spawnRate);
-        int index = Random.Range(0, targets.Count);
-        Instantiate(targets[index]);
-       }
-    }
+   {
+    while(true)
+   {
+    yield return new WaitForSeconds(spawnRate);
+    int index = Random.Range(0, targets.Count);
+    Instantiate(targets[index]);
+   }
+   }
 
-    private void UpdateScore(int scoreToAdd)
-    {
-       score += scoreToAdd;
-      scoreText.text = "Score" + score;
-    }
+    public void UpdateScore(int scoreToAdd)
+   {
+    score += scoreToAdd;
+    scoreText.text = "Score" + score;
+   }
 }
--- CreateWithCode/Prototype 5/Assets/Scripts/Target.cs
+++ CreateWithCode/Prototype 5/Assets/Scripts/Target.cs
@@ -11,11 +11,13 @@ public class Target : MonoBehaviour
     private float xRange = 4;
     private float ySpawnPos = -6;
     private GameManager gameManager;
+    public int pointValue;
 
     // Start is called before the first frame update
     void Start()
     {
        targetRb = GetComponent<Rigidbody>();
+       gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(),ForceMode.Impulse);
        
@@ -43,6 +45,7 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
      Destroy(gameObject);
+     gameManager.UpdateScore(5);
     }
 
     private void OnTriggerEnter(Collider other)
